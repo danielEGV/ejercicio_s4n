9e9d87580c420233a04d0d0836d57f60a8bb50df
danielEGV <deguevarav@unal.edu.co>
2018-06-19 17:20:40 -0500

    Mockito

---
+++ src/test/java/co/com/s4n/semillero/ejercicio/otherTest/testMockito.java
@@ -0,0 +1,61 @@
+package co.com.s4n.semillero.ejercicio.otherTest;
+
+
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Calculator {
+
+    public int add(int a, int b) {
+        return MathUtil.addInteger(a, b);
+    }
+}
+
+abstract class MathUtil {
+
+    public static final int addInteger(int a, int b) {
+        return a + b;
+    }
+
+    private MathUtil() {}
+}
+
+@RunWith(JUnitPlatform.class)
+@PrepareForTest(MathUtil.class)
+public class testMockito {
+
+    private Calculator calculator;
+
+
+    public void setUp() {
+        calculator = new Calculator();
+
+        PowerMockito.mockStatic(MathUtil.class);
+        PowerMockito.when(MathUtil.addInteger(1, 1)).thenReturn(0);
+        PowerMockito.when(MathUtil.addInteger(2, 2)).thenReturn(1);
+    }
+
+    @Test
+    public void shouldCalculateInAStrangeWay() {
+        calculator = new Calculator();
+        PowerMockito.spy(MathUtil.class);
+        PowerMockito.verifyStatic(MathUtil.class);
+        //assertEquals(0, calculator.add(1, 1));
+        //assertEquals(1, calculator.add(2, 2));
+    }
+
+
+
+
+}
---
+++ src/test/java/co/com/s4n/semillero/ejercicio/otherTest/testPrueba.java
@@ -0,0 +1,16 @@
+package co.com.s4n.semillero.ejercicio.otherTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(JUnitPlatform.class)
+public class testPrueba {
+
+    @Test
+    public void mokeTest() {
+        assertTrue(true);
+    }
+}
